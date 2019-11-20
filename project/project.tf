c293c8ae35598c3716bb05750c4563fc4dfb3495
Danny Sievers <danjsievers@gmail.com>
2019-11-20 15:30:26 -0600

    change interpolation to 0.12 syntax

--- project/project.tf
+++ project/project.tf
@@ -4,13 +4,13 @@ resource "random_id" "id" {
 }
 
 resource "google_project" "project" {
- name            = "${var.project_name}"
- project_id      = "${random_id.id.hex}"
- billing_account = "${var.billing_account_id}"
- org_id          = "${var.org_id}"
+ name            = "var.project_name"
+ project_id      = "random_id.id.hex"
+ billing_account = "var.billing_account_id"
+ org_id          = "var.org_id"
 }
 
 resource "google_project_services" "project" {
- project = "${google_project.project.project_id}"
- services = "${var.gcp_services}"
+ project = "google_project.project.project_id"
+ services = "var.gcp_services"
 }
