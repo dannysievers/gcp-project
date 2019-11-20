158116c163d7ccc0e70dd1bbf78c3788e6a2a7be
Danny Sievers <danjsievers@gmail.com>
2019-11-20 17:16:09 -0600

    fixing 0.12 style interpolation

--- project/project.tf
+++ project/project.tf
@@ -4,14 +4,14 @@ resource "random_id" "id" {
 }
 
 resource "google_project" "project" {
-  name            = "${var.project_name}"
-  project_id      = "${random_id.id.hex}"
-  billing_account = "var.billing_account_id"
-  org_id          = "var.org_id"
+  name            = var.project_name
+  project_id      = random_id.id.hex
+  billing_account = var.billing_account_id
+  org_id          = var.org_id
 }
 
 resource "google_project_service" "project" {
   count = length(var.gcp_services)
-  project = "google_project.project.project_id"
-  service = "var.gcp_services[count.index]"
+  project = google_project.project.project_id
+  service = var.gcp_services[count.index]
 }
