8d6862949edae43872666a29662b1fbc7c461ea4
Danny Sievers <danjsievers@gmail.com>
2019-11-20 16:05:33 -0600

    testing interpolation for random id resource

--- project/project.tf
+++ project/project.tf
@@ -5,7 +5,7 @@ resource "random_id" "id" {
 
 resource "google_project" "project" {
   name            = "var.project_name"
-  project_id      = "random_id.id.hex"
+  project_id      = "${random_id.id.hex}"
   billing_account = "var.billing_account_id"
   org_id          = "var.org_id"
 }
