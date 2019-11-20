74d77e3c5fe0fa5cdd2e2e459b88e706bffde798
Danny Sievers <danjsievers@gmail.com>
2019-11-20 15:58:58 -0600

    update project to iterate over services and use 3.0.0 google_project_service

--- project/project.tf
+++ project/project.tf
@@ -1,16 +1,17 @@
 resource "random_id" "id" {
- byte_length = 4
- prefix      = "${var.project_name}-"
+  byte_length = 4
+  prefix      = "${var.project_name}-"
 }
 
 resource "google_project" "project" {
- name            = "var.project_name"
- project_id      = "random_id.id.hex"
- billing_account = "var.billing_account_id"
- org_id          = "var.org_id"
+  name            = "var.project_name"
+  project_id      = "random_id.id.hex"
+  billing_account = "var.billing_account_id"
+  org_id          = "var.org_id"
 }
 
-resource "google_project_services" "project" {
- project = "google_project.project.project_id"
- services = "var.gcp_services"
+resource "google_project_service" "project" {
+  count = length(var.gcp_services)
+  project = "google_project.project.project_id"
+  services = "var.gcp_services[count.index]"
 }
